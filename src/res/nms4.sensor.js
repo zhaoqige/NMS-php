840d66e2b5d667a8e4dfe28dd33a934389e7753d
zhaoqige@163.com <Qige@6Harmonics>
2016-12-29 09:54:31 +0800

    fix issue #1 by Qige @ 2016.12.29

--- src/res/nms4.sensor.js
+++ src/res/nms4.sensor.js
@@ -72,16 +72,15 @@ var _author = 'Designed by 6WiLink Qige', _address = 'Address: Suit 3B-1102/1105
           $.app.error('File Format Invalid');	
           _mapConfig.points = null;
         }
-        
+                
+        // clear & add new icons
+        $.MicrosoftMap.sync(_mapConfig.points); 
         // move & zoom
         if (_currentSensorSN == '') {
           $.MicrosoftMap.setView({ center: _mapConfig.center, zoom: _mapConfig.zoomLevel });
           _currentSensorSN = ' ';
         }
       },'json');
-        
-      // clear & add new icons
-      $.MicrosoftMap.sync(_mapConfig.points); 
     }
 	};
 }) (jQuery);
